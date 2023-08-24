9e3f2ad3c1a6314ad7ef570415d18e1d90067ccd
GalvisDeveloper <galvisdeveloper@gmail.com>
2023-08-23 22:53:42 -0500

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+
+const App = () => {
+
+  return (
+    <View >
+      <Text>Rendering</Text>
+      <TouchableOpacity >
+        <Text>Rendering gonorrea</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default App

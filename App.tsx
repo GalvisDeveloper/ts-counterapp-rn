3046f8ce6c5df56ddcf0350e6089ddf0b7c28a10
GalvisDeveloper <galvisdeveloper@gmail.com>
2023-08-29 21:37:03 -0500

    Removing src

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Text, TouchableOpacity, View } from 'react-native'
-import { CounterScreen } from './src/src/screens/CounterScreen'
+import { CounterScreen } from './src/screens/CounterScreen'
 
 const App = () => {
 
